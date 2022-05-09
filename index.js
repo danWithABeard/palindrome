0a158cfc4391a378aa66a5ea7642a31ed49f070d
DWAB Cicconi <danielc@tradecoffeeco.com>
2022-05-09 00:55:37 -0400

    Add comments

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/**
+ * isPalindrome
+ *
+ * When given a string, this function determines if the string is spelled the same backwards and forwards.
+ *
+ * @param str string
+ * @returns boolean
+ */
 export function isPalindrome(str) {
 
 }
